d863914369d1fed4417dce46919f6336eee9ef31
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-04-19 14:20:20 -0400

    Added Card Class

---
+++ MyAPI/MyAPI/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace MyAPI.Models
+{
+    public class Card
+    {
+        public int card_id { get; set; }
+        public string cvv { get; set; }
+        public string expiration { get; set; }
+        public string numbers { get; set; }
+    }
+}
