b94acbbaf91ec546daeec9b17a3d58504fd5e6b9
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-04-19 14:16:28 -0400

    Created User class

---
+++ MyAPI/MyAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MyAPI.Models
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string pass { get; set; }
+        public string email { get; set; }
+        public float balance { get; set; }
+        public int card_id { get; set; }
+    }
+}
