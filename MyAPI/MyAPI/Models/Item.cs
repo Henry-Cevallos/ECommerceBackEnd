27f6d99fca7f7b1382a99a010461bb35a847f529
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-04-19 14:22:45 -0400

    Added item class

---
+++ MyAPI/MyAPI/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAPI.Models
+{
+    public class Item
+    {
+        public int item_id { get; set; }
+        public int user_id { get; set; }
+        public string title { get; set; }
+        public float price { get; set; }   
+        public string description { get; set; }
+    }
+}
